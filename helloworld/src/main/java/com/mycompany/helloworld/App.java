daf450e05cf9579e033f82cbee4644ffaf9c0490
goldeneyezhang <goldeneyezhang@163.com>
2017-01-10 16:48:06 +0800

    new

--- helloworld/src/main/java/com/mycompany/helloworld/App.java
+++ helloworld/src/main/java/com/mycompany/helloworld/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! 张一博" );
+        App test=new App();
+        test.pupAge();
+    }
+    public void pupAge()
+    {
+    	int age=0;
+    	age=age+7;
+    	System.out.println("小狗的年龄是:"+age);
     }
 }
